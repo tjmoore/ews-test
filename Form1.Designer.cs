2f0a89ec666d4dfa0bdad8043cbe4dfad09fba74
Tim Moore <tim.moore@ae35.co.uk>
2017-04-23 17:18:51 +0100

    Initial implementation

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.buttonAutoDiscover = new System.Windows.Forms.Button();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBoxAllowHttp = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(95, 34);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(291, 20);
+            this.textBoxEmail.TabIndex = 0;
+            this.textBoxEmail.TextChanged += new System.EventHandler(this.textBoxEmail_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Email Address:";
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxLog.Location = new System.Drawing.Point(13, 115);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxLog.Size = new System.Drawing.Size(613, 263);
+            this.textBoxLog.TabIndex = 4;
+            this.textBoxLog.WordWrap = false;
+            // 
+            // buttonAutoDiscover
+            // 
+            this.buttonAutoDiscover.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAutoDiscover.Location = new System.Drawing.Point(519, 32);
+            this.buttonAutoDiscover.Name = "buttonAutoDiscover";
+            this.buttonAutoDiscover.Size = new System.Drawing.Size(107, 23);
+            this.buttonAutoDiscover.TabIndex = 2;
+            this.buttonAutoDiscover.Text = "&Auto Discover";
+            this.buttonAutoDiscover.UseVisualStyleBackColor = true;
+            this.buttonAutoDiscover.Click += new System.EventHandler(this.buttonAutoDiscover_Click);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(95, 61);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(291, 20);
+            this.textBoxPassword.TabIndex = 1;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            this.textBoxPassword.TextChanged += new System.EventHandler(this.textBoxPassword_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password:";
+            // 
+            // checkBoxAllowHttp
+            // 
+            this.checkBoxAllowHttp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBoxAllowHttp.AutoSize = true;
+            this.checkBoxAllowHttp.Location = new System.Drawing.Point(513, 64);
+            this.checkBoxAllowHttp.Name = "checkBoxAllowHttp";
+            this.checkBoxAllowHttp.Size = new System.Drawing.Size(113, 17);
+            this.checkBoxAllowHttp.TabIndex = 3;
+            this.checkBoxAllowHttp.Text = "Allow HTTP URLs";
+            this.checkBoxAllowHttp.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.buttonAutoDiscover;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(638, 390);
+            this.Controls.Add(this.checkBoxAllowHttp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.buttonAutoDiscover);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxEmail);
+            this.MinimumSize = new System.Drawing.Size(531, 400);
+            this.Name = "Form1";
+            this.Text = "EWS Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.Button buttonAutoDiscover;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBoxAllowHttp;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,220 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Exchange.WebServices.Autodiscover;
+using Microsoft.Exchange.WebServices.Data;
+using System;
+using System.Globalization;
+using System.Net;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace ews_test
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form, ITraceListener
     {
+        private bool _running;
+        private bool _allowHttpUrls;
+
         public Form1()
         {
             InitializeComponent();
+            EnableButtons();
+
+            ServicePointManager.ServerCertificateValidationCallback = CertificateValidationCallBack;
         }
+
+        private async void buttonAutoDiscover_Click(object sender, EventArgs e)
+        {
+            textBoxLog.Clear();
+            _allowHttpUrls = checkBoxAllowHttp.Checked;
+
+            _running = true;
+            EnableButtons();
+
+            await System.Threading.Tasks.Task.Run(() => AutoDiscover());
+
+            _running = false;
+            EnableButtons();
+        }
+
+        private void textBoxEmail_TextChanged(object sender, EventArgs e)
+        {
+            EnableButtons();
+        }
+
+        private void textBoxPassword_TextChanged(object sender, EventArgs e)
+        {
+            EnableButtons();
+        }
+
+        private void EnableButtons()
+        {
+            buttonAutoDiscover.Enabled = _running == false && string.IsNullOrEmpty(textBoxEmail.Text) == false;
+        }
+
+        public void AutoDiscover()
+        {
+            string emailAddress = textBoxEmail.Text.Trim();
+
+            ExchangeCredentials credential = null;
+            if (string.IsNullOrEmpty(textBoxPassword.Text) == false)
+            {
+                string password = textBoxPassword.Text.Trim();
+                credential = new NetworkCredential(emailAddress, password);
+            }
+
+            ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010, TimeZoneInfo.Utc);
+
+            if (credential != null)
+            {
+                service.Credentials = credential;
+            }
+
+            try
+            {
+                // Auto-discover works for both on-prem an cloud Office 365 accounts
+                LogMessage(string.Format(CultureInfo.InvariantCulture,
+                        "Attempting to auto-discover Exchange Server for account '{0}'...", emailAddress));
+
+                // SCP lookup is slow
+                service.EnableScpLookup = false;
+
+                service.TraceFlags = TraceFlags.All;
+                service.TraceEnabled = true;
+                service.TraceEnablePrettyPrinting = true;
+                service.TraceListener = this;
+
+                // Attempt autodiscovery
+                service.AutodiscoverUrl(emailAddress, RedirectionUrlValidationCallback);
+
+                service.TraceEnabled = false;
+            }
+            catch (AutodiscoverLocalException ex)
+            {
+                LogMessage($"AutodiscoverLocalException: {ex.Message}");
+            }
+            catch (AutodiscoverRemoteException ex)
+            {
+                LogMessage($"AutodiscoverRemoteException: {ex.Message}");
+            }
+            catch (ServiceLocalException ex)
+            {
+                LogMessage($"ServiceLocalException: {ex.Message}");
+            }
+
+            if (service.Url == null)
+            {
+                LogMessage($"{Environment.NewLine}Unable to auto-discover URL");
+            }
+            else
+            {
+                LogMessage($"{Environment.NewLine}Discovered Exchange URL: {service.Url}");
+            }
+        }
+
+
+        private void LogMessage(string message)
+        {
+            textBoxLog.Invoke(new Action(()=> textBoxLog.AppendText(message + Environment.NewLine)));
+        }
+
+        private bool RedirectionUrlValidationCallback(string redirectionUrl)
+        {
+            // The default for the validation callback is to reject the URL.
+            bool result = false;
+
+            Uri redirectionUri = new Uri(redirectionUrl);
+
+            // Validate the contents of the redirection URL. In this simple validation
+            // callback, the redirection URL is considered valid if it is using HTTPS
+            // to encrypt the authentication credentials. 
+            if (redirectionUri.Scheme == "https")
+            {
+                result = true;
+            }
+            else if (redirectionUri.Scheme == "http" && _allowHttpUrls)
+            {
+                result = true;
+            }
+
+            string valid = result ? "Valid" : "Invalid";
+            LogMessage($"RedirectionUrlValidationCallback - {valid} URL");
+
+            return result;
+        }
+
+        public void Trace(string traceType, string traceMessage)
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(traceMessage);
+
+                XmlNodeList commandNodes = doc.GetElementsByTagName("Trace");
+                foreach (XmlNode node in commandNodes)
+                {
+                    LogMessage($"EWS Autodiscover: {node.InnerText.Replace("\r", "").Replace("\n", "")}");
+                }
+            }
+            catch (XmlException)
+            {
+                // Unfortunately, sometimes the trace XML is corrupted by improper encoding of an 
+                // embedded SOAP message.
+                LogMessage($"EWS Autodiscover: {traceType}");
+            }
+        }
+
+
+        private bool CertificateValidationCallBack(
+            object sender,
+            System.Security.Cryptography.X509Certificates.X509Certificate certificate,
+            System.Security.Cryptography.X509Certificates.X509Chain chain,
+            System.Net.Security.SslPolicyErrors sslPolicyErrors)
+        {
+            // If the certificate is a valid, signed certificate, return true.
+            if (sslPolicyErrors == System.Net.Security.SslPolicyErrors.None)
+            {
+                LogMessage("CertificateValidationCallBack: SSL Certificate valid");
+                return true;
+            }
+
+            // If there are errors in the certificate chain, look at each error to determine the cause.
+            if ((sslPolicyErrors & System.Net.Security.SslPolicyErrors.RemoteCertificateChainErrors) != 0)
+            {
+                if (chain != null && chain.ChainStatus != null)
+                {
+                    foreach (System.Security.Cryptography.X509Certificates.X509ChainStatus status in chain.ChainStatus)
+                    {
+                        if ((certificate.Subject == certificate.Issuer) &&
+                           (status.Status == System.Security.Cryptography.X509Certificates.X509ChainStatusFlags.UntrustedRoot))
+                        {
+                            // Self-signed certificates with an untrusted root are valid. 
+                            LogMessage("CertificateValidationCallBack: SSL Certificate valid - self signed with untrusted root, treating as valid");
+                            continue;
+                        }
+                        else
+                        {
+                            if (status.Status != System.Security.Cryptography.X509Certificates.X509ChainStatusFlags.NoError)
+                            {
+                                // If there are any other errors in the certificate chain, the certificate is invalid,
+                                // so the method returns false.
+                                LogMessage($"CertificateValidationCallBack: SSL Certificate invalid - errors in certificate chain: {status.StatusInformation}");
+                                return false;
+                            }
+                        }
+                    }
+                }
+
+                // When processing reaches this line, the only errors in the certificate chain are 
+                // untrusted root errors for self-signed certificates. These certificates are valid
+                // for default Exchange server installations, so return true.
+                return true;
+            }
+            else
+            {
+                // In all other cases, return false.
+                LogMessage($"CertificateValidationCallBack: SSL Certificate invalid: {sslPolicyErrors}");
+                return false;
+            }
+        }
+
     }
 }
